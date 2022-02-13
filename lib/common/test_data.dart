8d4ad2ae010320f8fd114a57fb2820eb3c08d47d
tokku5552 <physics.0121@gmail.com>
2022-02-13 23:07:43 +0900

    implement: model & test data

---
+++ lib/common/test_data.dart
@@ -0,0 +1,8 @@
+import 'package:todoapp_sample_riverpod/model/todo_item.dart';
+
+List<TodoItem> getTestTodoList = [
+  TodoItem(title: 'test001', detail: 'test001detail', isDone: false),
+  TodoItem(title: 'test002', detail: 'test002detail', isDone: false),
+  TodoItem(title: 'test003', detail: 'test003detail', isDone: false),
+  TodoItem(title: 'test004', detail: 'test004detail', isDone: false)
+];
---
+++ lib/model/todo_item.dart
@@ -0,0 +1,11 @@
+class TodoItem {
+  TodoItem({
+    required this.title,
+    required this.detail,
+    required this.isDone,
+  });
+
+  String title;
+  String detail;
+  bool isDone;
+}
