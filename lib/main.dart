bf04fd815af034040e0300a63f314d939ca2f242
tokku5552 <physics.0121@gmail.com>
2022-02-14 23:56:41 +0900

    add firebase initialization code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:todoapp_sample_riverpod/page/todo_list_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const App());
 }
 
