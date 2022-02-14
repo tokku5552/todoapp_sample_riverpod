6d5f8bb560e1feb06b006f76018067d8b96bc7ae
tokku5552 <physics.0121@gmail.com>
2022-02-15 08:31:26 +0900

    data構造修正　及びpubspec.yamlの不要なコメントアウト削除

--- lib/common/test_data.dart
+++ lib/common/test_data.dart
@@ -1,8 +1,24 @@
 import 'package:todoapp_sample_riverpod/model/todo_item.dart';
 
 List<TodoItem> getTestTodoList = [
-  TodoItem(title: 'test001', detail: 'test001detail', isDone: false),
-  TodoItem(title: 'test002', detail: 'test002detail', isDone: false),
-  TodoItem(title: 'test003', detail: 'test003detail', isDone: false),
-  TodoItem(title: 'test004', detail: 'test004detail', isDone: false)
+  TodoItem(
+      id: '0000000000000001',
+      title: 'test001',
+      detail: 'test001detail',
+      isDone: false),
+  TodoItem(
+      id: '0000000000000002',
+      title: 'test002',
+      detail: 'test002detail',
+      isDone: false),
+  TodoItem(
+      id: '0000000000000003',
+      title: 'test003',
+      detail: 'test003detail',
+      isDone: false),
+  TodoItem(
+      id: '0000000000000004',
+      title: 'test004',
+      detail: 'test004detail',
+      isDone: false)
 ];
--- lib/model/todo_item.dart
+++ lib/model/todo_item.dart
@@ -1,10 +1,12 @@
 class TodoItem {
   TodoItem({
+    required this.id,
     required this.title,
     required this.detail,
     required this.isDone,
   });
 
+  String id;
   String title;
   String detail;
   bool isDone;
