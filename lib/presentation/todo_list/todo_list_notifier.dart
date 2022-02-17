09f64276a8596ae3f868cb19507beefd7787b9d3
tokku5552 <physics.0121@gmail.com>
2022-02-18 00:03:05 +0900

    repositoryとstateを実装

---
+++ lib/presentation/todo_list/todo_list_notifier.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todoapp_sample_riverpod/infrastructure/todo_item_repository.dart';
+import 'package:todoapp_sample_riverpod/presentation/todo_list/todo_list_state.dart';
+
+final todoListStateProvider =
+    StateNotifierProvider<TodoListNotifier, TodoListState>(
+  (ref) => TodoListNotifier(
+    todoItemRepository: ref.read(todoItemRepository),
+  )..init(),
+);
+
+class TodoListNotifier extends StateNotifier<TodoListState> {
+  TodoListNotifier({required TodoItemRepository todoItemRepository})
+      : _todoItemRepository = todoItemRepository,
+        super(const TodoListState());
+
+  final TodoItemRepository _todoItemRepository;
+
+  Future<void> init() async {
+    state = state.copyWith(isFetching: true);
+    final list = await _todoItemRepository.findAll();
+    state.copyWith(todoList: list, isFetching: false);
+  }
+}
