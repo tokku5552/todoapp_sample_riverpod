6508dd7f078e5ba74dd0a6a8b145939c61b1d959
tokku5552 <physics.0121@gmail.com>
2022-02-18 00:50:28 +0900

    wip : 非同期で最初にアイテムを取ってくる処理がうまくいってない

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todoapp_sample_riverpod/presentation/todo_list/todo_list_page.dart';
 
-void main() async {
+import 'infrastructure/todo_item_repository.dart';
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const App());
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
 }
 
 class App extends StatelessWidget {
--- lib/presentation/todo_list/todo_list_page.dart
+++ lib/presentation/todo_list/todo_list_page.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todoapp_sample_riverpod/common/test_data.dart';
+import 'package:todoapp_sample_riverpod/presentation/todo_list/todo_list_notifier.dart';
 
-class TodoListPage extends StatelessWidget {
+class TodoListPage extends ConsumerWidget {
   const TodoListPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(todoListStateProvider);
+    final notifier = ref.watch(todoListStateProvider.notifier)..init();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Todo List Page'),
       ),
       body: ListView(
-        children: getTestTodoList
+        children: state.todoList
             .map(
               (item) => ListTile(
                 leading: Checkbox(
